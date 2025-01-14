a8625f71d90bc48ce4499d5976e5c64e5f090466
Elina Toivonen <elina.97@hotmail.com>
2025-01-14 13:15:04 +0200

    a reactin alkeet done

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old.</p>
+    </div>
+  )
+}
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const nimi = 'Pekka'
+  const ika = 10
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Greetings</h1>
+      <Hello name="Maija" age={26 + 10} />
+      <Hello name={nimi} age={ika} />
+      <Footer />
     </>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by
+      <a href="https://github.com/Ellureb">Ellureb</a>
+    </div>
   )
 }
 
