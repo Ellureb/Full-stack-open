1cbeebfc52e310e8da98e55bdb29fb128f82fab0
Elina Toivonen <elina.97@hotmail.com>
2025-01-14 17:33:22 +0200

    1.2 kurssitiedot, step2

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -32,9 +32,9 @@ const Content = (props) => {
   console.log(props)
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   )
 }
@@ -45,6 +45,12 @@ const Total = (props) => {
     <div>
       <p>Number of exercises {total}</p>
     </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
   )
 }
 
