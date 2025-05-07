75648f2d8b89b79d68c2c4e01292a0eda5244524
Ellureb <98943247+Ellureb@users.noreply.github.com>
2025-05-07 19:32:02 +0300

    1.8: unicafe step3

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -7,8 +7,21 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = (props) => {
+  return(
+    <div>
+      <h2>statistics</h2>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.total}</p>
+      <p>average {props.average}</p>
+      <p>positive {props.positive} %</p>
+    </div>
+  )
+}
+
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -23,13 +36,7 @@ const App = () => {
       <Button onClick={() => setGood(good + 1)} text='good'/>
       <Button onClick={() => setNeutral(neutral + 1)} text='neutral'/>
       <Button onClick={() => setBad(bad + 1)} text='bad'/>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} positive={positive}/>
     </div>
   )
 }
