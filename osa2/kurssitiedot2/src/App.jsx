cee7ef90ec58637ee714498ba9aaa0116112992e
Ellureb <98943247+Ellureb@users.noreply.github.com>
2025-06-23 16:27:28 +0300

    2.5: erillinen moduuli

--- osa2/kurssitiedot2/src/App.jsx
+++ osa2/kurssitiedot2/src/App.jsx
@@ -1,46 +1,4 @@
-const Header = ({ course }) => {
-  return (
-    <div>
-      <h1>{course.name}</h1>
-    </div>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part =>
-        <Part key={part.id} part={part}/>
-      )}
-    </div>
-  )
-}
-
-const Part = ({ part}) => {
-  return (
-    <p>{part.name} {part.exercises}</p>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((acc, part) => acc + part.exercises, 0);
-
-  return (
-    <div>
-      <p>Number of exercises: {total}</p>
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from './Course'
 
 const App = () => {
   const courses = [
