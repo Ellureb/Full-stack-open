8610ff79bb4e9de5afd91ade3c0794d3e5d014e0
Ellureb <98943247+Ellureb@users.noreply.github.com>
2025-07-02 10:43:19 +0300

    maiden tiedot, välistep

--- osa2/maiden-tiedot/src/App.jsx
+++ osa2/maiden-tiedot/src/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from 'react'
+import Filter from './components/Filter'
 
 function App() {
+  const [filter, setFilter] = useState('')
+
+  const filteredCountries = filter
+    ? countries.filter(country =>
+        country.name.toLowerCase().includes(filter.toLowerCase())
+      )
+    : []
+
+  const showCountries = filter && filteredCountries.length > 10
+    ? "Too many matches, specify another filter"
+    : filteredCountries
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
 
   return (
     <div>
       <h1>Welcome to country finder!</h1>
+      <Filter value={filter} onChange={handleFilterChange} />
     </div>
   )
 }
