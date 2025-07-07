1049c900a56520232e2e9a13ebbed9914fa23375
Ellureb <98943247+Ellureb@users.noreply.github.com>
2025-07-07 14:57:33 +0300

    3.2: puhelinluettelon backend, step2

--- osa3/puhelinluettelon-backend/index.js
+++ osa3/puhelinluettelon-backend/index.js
@@ -30,6 +30,16 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    const currentTime = new Date()
+    const info = `
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${currentTime}</p>
+    `
+    
+    res.send(info)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
