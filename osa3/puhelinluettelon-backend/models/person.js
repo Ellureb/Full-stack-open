ee49db4466c8fabea7fa242cf6ef8d11a025e84b
Ellureb <98943247+Ellureb@users.noreply.github.com>
2026-01-23 11:20:15 +0200

    3.22: lint-konfiguraatio

--- osa3/puhelinluettelon-backend/models/person.js
+++ osa3/puhelinluettelon-backend/models/person.js
@@ -25,7 +25,7 @@ const personSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator: function(v) {
-        return /^\d{2,3}-\d+$/.test(v);
+        return /^\d{2,3}-\d+$/.test(v)
       },
       message: props => `${props.value} is not a valid phone number`
     }
@@ -33,11 +33,11 @@ const personSchema = new mongoose.Schema({
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
--- osa3/puhelinluettelon-backend/mongo.js
+++ osa3/puhelinluettelon-backend/mongo.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 if (process.argv.length !== 3 && process.argv.length !== 5) {
-    console.log('How to use:')
-    console.log('Log all database entries: node mongo.js <password>')
-    console.log('Add new person to phonebook: node mongo.js <password> <name> <number>')
-    process.exit(1)
+  console.log('How to use:')
+  console.log('Log all database entries: node mongo.js <password>')
+  console.log('Add new person to phonebook: node mongo.js <password> <name> <number>')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -22,28 +22,28 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url, { family: 4 })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-    console.log('Phonebook: ')
-    Person.find({}).then(result => {
-      result.forEach(person => {
-        console.log(`${person.name} ${person.number}`)
-      })
-      mongoose.connection.close()
-    }) 
+  console.log('Phonebook: ')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
 } else if (process.argv.length === 5) {
-    const person = new Person({
-        name: name,
-        number: number,
-    })
+  const person = new Person({
+    name: name,
+    number: number,
+  })
 
-    person.save().then(result => {
-        console.log(`Added ${name} ${number} to phonebook`)
-        mongoose.connection.close()
-    })
+  person.save().then(result => {
+    console.log(`Added ${name} ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
