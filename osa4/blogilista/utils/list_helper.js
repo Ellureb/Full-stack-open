52d8f8fa81b505438b6e4895cf2881ab75ec7f0e
Ellureb <98943247+Ellureb@users.noreply.github.com>
2026-02-19 13:10:47 +0200

    4.5*: apufunktioita ja yksikkötestejä, step3

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -6,7 +6,14 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((fave, blog) => {
+        return blog.likes > fave.likes ? blog : fave
+    })
+}
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 }
