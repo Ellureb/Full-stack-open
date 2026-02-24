43f7a757d7e0b3e6a1e98bff84f8febfe86afc75
Ellureb <98943247+Ellureb@users.noreply.github.com>
2026-02-24 20:32:11 +0200

    4.9: blogilistan testit, step2

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -7,6 +7,26 @@ const Blog = require('../models/blog')
 
 const api = supertest(app)
 
+const initialBlogs = [
+    {
+        title: "SipsiHullu",
+        author: "Estrella Taffel",
+        url: "www.sipsihullu.fi",
+        likes: 243,
+    },
+    {
+        title: "Bellan Blogi",
+        author: "Bella Bloggaaja",
+        url: "www.blogijee.com",
+        likes: 5,
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(initialBlogs)
+})
+
 test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
@@ -17,7 +37,17 @@ test('blogs are returned as json', async () => {
 test('all blogs are returned', async () => {
     const response = await api.get('/api/blogs')
 
-    assert.strictEqual(response.body.length, 2)
+    assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+test('id field must be named id', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+
+    const blog = response.body[0]
+    assert(blog.id) // löytyy id
+    assert.strictEqual(blog._id, undefined) // ei löydy _id
 })
 
 after(async () => {
