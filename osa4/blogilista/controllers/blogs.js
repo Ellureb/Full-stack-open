08095b4c7515023ea622306c74f92dcfbc83bee7
Ellureb <98943247+Ellureb@users.noreply.github.com>
2026-03-14 13:37:53 +0200

    4.15: blogilistan laajennus, step3

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,6 +1,5 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const { nonExistingId } = require('../tests/test_helper')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
