c6ab77f8cff6cf136a8b5d28ae5f6be75d8fed0d
Ellureb <98943247+Ellureb@users.noreply.github.com>
2026-04-09 20:16:04 +0300

    5.7: blogilistan frontend, step7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showInfo, setShowInfo] = useState(false)
+
+  return (
+    <div style={{
+      paddingTop: 2,
+      paddingLeft: 10,
+      border: '2px solid rgb(137, 46, 173)',
+    }}>
+      {blog.title} - {blog.author}
+      <button onClick={() => setShowInfo(!showInfo)}>{showInfo ? 'show less' : 'show more'}</button>
+      {showInfo && (
+        <div>
+          <div>url: {blog.url}</div>
+          <div>
+            likes: {blog.likes}
+            <button>like</button>
+          </div>
+          <div>
+            added by: {blog.user.name}
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
