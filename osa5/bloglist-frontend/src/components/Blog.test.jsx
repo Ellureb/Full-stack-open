b6790c8bdb0cf0c15bb8bd3722aa89f508ec2bc0
Ellureb <98943247+Ellureb@users.noreply.github.com>
2026-05-15 15:09:39 +0300

    5.15: blogilistan testit, step3

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -34,9 +34,36 @@ test('after clicking the button, url, likes and user is showed', async () => {
     const button = screen.getByText('show more')
     await user.click(button)
 
-    screen.debug()
-
     expect(screen.getByText('url: www.test.fi')).toBeVisible()
     expect(screen.getByText('likes: 5')).toBeVisible()
     expect(screen.getByText('added by: Test User')).toBeVisible()
+})
+
+test('clicking the like button twice calls event handler twice', async () => {
+    const blog = {
+        title: 'Test blog',
+        author: 'Testi',
+        url: 'www.test.fi',
+        likes: 5,
+        user: {
+            name: 'Test User'
+        }
+    }
+
+    const mockHandler = vi.fn()
+
+    render(
+        <Blog blog={blog} handleLikeClick={mockHandler} />
+    )
+
+    const user = userEvent.setup()
+
+    const showButton = screen.getByText('show more')
+    await user.click(showButton)
+
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
 })
