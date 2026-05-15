3da4361b725771237d35d3a68e8f86e150ddfa0b
Ellureb <98943247+Ellureb@users.noreply.github.com>
2026-05-15 14:52:27 +0300

    5.14: blogilistan testit, step2

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders blog title', () => {
@@ -10,9 +11,32 @@ test('renders blog title', () => {
 
     render(<Blog blog={blog} />)
 
-    screen.debug()
-
     const element = screen.getByText(/This is a test to render blog title/)
 
     expect(element).toBeDefined()
+})
+
+test('after clicking the button, url, likes and user is showed', async () => {
+    const blog = {
+        title: 'Test blog',
+        author: 'Testi',
+        url: 'www.test.fi',
+        likes: 5,
+        user: {
+            name: 'Test User'
+        }
+    }
+    
+    const user = userEvent.setup()
+
+    render(<Blog blog={blog} />)
+
+    const button = screen.getByText('show more')
+    await user.click(button)
+
+    screen.debug()
+
+    expect(screen.getByText('url: www.test.fi')).toBeVisible()
+    expect(screen.getByText('likes: 5')).toBeVisible()
+    expect(screen.getByText('added by: Test User')).toBeVisible()
 })
